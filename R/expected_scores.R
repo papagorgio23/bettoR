fc75b80db84f50a6d410587b3358517b895330d1
Jason Lee <Jason@AISportsFirm.com>
2020-07-24 22:50:58 -0700

    Expected Scores

---
+++ R/expected_scores.R
@@ -0,0 +1,30 @@
+#' @title Expected Scores
+#'
+#' @description This function calculates the expected scores for a given game.
+#'
+#' @param home_line Line for the home team (-6)
+#' @param total Total for the game (212)
+#'
+#' @return dataframe with expected home score and away score for the game
+#'
+#' @examples expected_scores(home_line = -6, total = 212)
+#' @examples expected_scores(home_line = -2.5, total = 51)
+#' @examples expected_scores(home_line = 10.5, total = 231)
+#'
+#' @export
+expected_scores <- function(home_line, total){
+  ## Error handling
+  if (!is.numeric(c(home_line, total))) {
+    stop("Inputs must be numeric")
+  }
+
+  # math
+  scores <- data.frame(
+    home_line = home_line,
+    total = total,
+    home_score = (total - home_line) / 2,
+    away_score = (total + home_line) / 2
+  )
+
+  return(scores)
+}
